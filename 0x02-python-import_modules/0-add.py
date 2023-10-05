f4b294295e2cc78938cbfe63185edac15057a64e
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-05 23:47:55 +0100

    Hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-# Import the add function from add_0.py
 from add_0 import add
-
-# Define variables a and b
 a = 1
 b = 2
-
-# Call add and display the result using string formatting
 print("{0} + {1} = {2}".format(a, b, add(a, b)))
