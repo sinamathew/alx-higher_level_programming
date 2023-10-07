8621daa9a0d0722c02e1061de568550cd8d72f14
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-07 23:00:40 +0100

    Function name

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     if (idx > len(my_list) - 1 or idx < 0):
         return my_list
     else:
