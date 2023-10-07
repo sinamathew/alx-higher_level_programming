6818005fe58e50e3ea8954e9024b2670f2d69003
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-08 00:38:30 +0100

    Tried my best

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in range(len(my_string)):
-        del my_string[i] if 'C' or 'c':
-
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            del my_string[i]
         else:
             i += 1
-
     return my_string
