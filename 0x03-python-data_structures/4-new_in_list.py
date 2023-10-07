ef8483f9fe12bb34364b753ed7a784ff246d49e6
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-08 00:01:46 +0100

    Test!

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (idx >= len(new_list) or idx < 0):
+    if (idx >= len(my_list) or idx < 0):
         return my_list[:]
     else:
         new_list = my_list[:]
