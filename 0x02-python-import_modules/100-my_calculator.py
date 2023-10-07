9aee3e7f6facca216c5a201d0ddde4eb391ef456
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-07 08:54:44 +0100

    Calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if (len(sys.argv) != 4):
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        exit 1
+    else:
+        a = sys.argv[1]
+        b = sys.argv[3]
+        operator = sys.argv[2]
+
+        if (operator = "+"):
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif (operator = "-"):
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif (operator = "*"):
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif (operator = "/"):
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        else:
+            print("Available operators: +, -, *, and /")
+            exit 1
