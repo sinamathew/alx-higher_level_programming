a907e4a4693f57ff4cab4a87cff413ad19ba9731
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-09 19:04:11 +0100

    Hot fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_result = ()
     for i in range(2):
         tuple_result(i) == tuple_a(i) + tuple_b(i)
     return tuple_result
