408458c6294b232a80612a2fd0ffeebc0cc5b7e3
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-09 18:36:40 +0100

    Seems it worked

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in matrix[i]:
+        for j in i:
             print("{}".format(j), end = " ")
         print()
