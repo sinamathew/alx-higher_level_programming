52c04fdb066aa914ebf6c615f1ca3c634f803377
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-10 00:03:18 +0100

    Efficient

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    for i in my_list:
-        biggest_int = 0
+    if not my_list:
+        return None
+
+    biggest_int = my_list[0]
+
+    for i in range(1, len(my_list)):
         if (my_list[i] > biggest_int):
             biggest_int = my_list[i]
+
     return biggest_int
