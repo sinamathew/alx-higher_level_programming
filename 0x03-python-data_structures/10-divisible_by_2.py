da4d401c76e8ccae3a31a28be4afaa82b47cad72
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-10 00:31:39 +0100

    What about now?

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list=[]
+    new_list = []
     for num in my_list:
-        if (num % 2 = 0):
+        if (num % 2 == 0):
             new_list += True
         else:
             new_list += False
