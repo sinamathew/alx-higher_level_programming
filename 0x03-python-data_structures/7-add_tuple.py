3d1bfe62df7afe55a06225e5f2ca4bcb5dd6b652
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-09 19:10:18 +0100

    Square brackets instead of parentheses

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_result = ()
     for i in range(2):
-        tuple_result(i) == tuple_a(i) + tuple_b(i)
+        tuple_result[i] == tuple_a[i] + tuple_b[i]
     return tuple_result
