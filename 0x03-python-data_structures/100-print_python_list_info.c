205db0e3c93890c4d1c99c14fe720968b629c52c
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-10 05:41:52 +0100

    God is the greatest

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+* print_python_list_info - Function name
+* @p: Parameter
+*
+* By: Sina Mathew
+*/
+void print_python_list_info(PyObject *p)
+{
+Py_ssize_t size, alloc, i;
+PyObject *item;
+
+size = PyList_Size(p);
+alloc = ((PyListObject *)p)->allocated;
+
+printf("[*] Size of the Python List = %ld\n", size);
+printf("[*] Allocated = %ld\n", alloc);
+
+for (i = 0; i < size; i++)
+{
+item = PyList_GetItem(p, i);
+printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+}
+}
+
