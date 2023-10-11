3eab2ce0d5118f830a3f1f56d61b38a3cf3dd2da
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-11 23:09:30 +0100

    God is good

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_set = []
+    total = 0
+    for num in my_list:
+        for num not in unique_list:
+            total += num
+        unique_list.append(num)
+    return total
