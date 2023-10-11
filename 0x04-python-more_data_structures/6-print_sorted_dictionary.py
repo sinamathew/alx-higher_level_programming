b3aef0887fd3cd8f5839d3f4463a23adb08bfb11
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-11 23:50:55 +0100

    Hard work pays

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dict = sorted(a_dictionary)
-    print(sorted_dict)
+    sorted_dict_keys = sorted(a_dictionary.keys())
+    for key in sorted_dict_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
