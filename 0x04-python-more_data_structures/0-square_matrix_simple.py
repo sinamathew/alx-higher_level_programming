0ccec6020171d5c1d42048d89ced3e31023d5f90
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-11 22:21:20 +0100

    All is well

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    new_row = []
+    for row in new_matrix:
+        for i in row:
+           new_row.append(i**i)
+        new_matrix.append(new_row)
