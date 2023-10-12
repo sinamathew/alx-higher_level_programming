8f021751143f959a58a20d608ff39c459ad84fe6
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-12 01:18:40 +0100

    Fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_key = None
-    for key in a_dictionary:
-        if a_dictionary[key] > best_key:
-            best_key = a_dictionary[key]
-    return best_key
+    if a_dictionary:
+        best_key = max(a_dictionary, key=a_dictionary.get)
+        return best_key
+    return None
