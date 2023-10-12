7fc2a67f366c15c658b336cd080d06a10fe9e01f
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-12 01:35:40 +0100

    Rome to Lagos

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for roman_char in reversed(roman_string):
+        if roman_char not in roman_dict:
+            return 0
+
+        current_value = roman_dict[roman_char]
+
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value
+
+        prev_value = current_value
+
+    return total
+
