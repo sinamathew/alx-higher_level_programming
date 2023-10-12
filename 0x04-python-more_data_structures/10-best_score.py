992e17efbb8c126054347fd1b2bb9015b676a991
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-12 01:08:33 +0100

    Indentation

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     best_key = 0
     for key in a_dictionary:
-        best_key = a_dictionary[key] if a_dictionary[key] > best_key
+        best_key = a_dictionary[key] if a_dictionary[key] > best_key:
     return best_key
