a83da41bedc86678612224be1c140043b8bf6293
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-23 23:17:19 +0100

    format specifier

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[1]), end="")
+            print("{:d}".format(my_list[1]), end="")
             i += 1
         except (ValueError, TypeError):
             break
