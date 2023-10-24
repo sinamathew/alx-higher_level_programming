99203e6cc4a1e7ded6762bd35da41c90e54048de
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-24 05:33:34 +0100

    no message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
-        return None
+        pass
     finally:
         print("{:d} / {:d} = {:d}".format(a, b, result))
     return result
