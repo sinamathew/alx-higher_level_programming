642cfed77480b0838cde02e48fb23ea640edc9f9
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-24 05:36:28 +0100

    no message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         pass
     finally:
-        print("{:d} / {:d} = {:d}".format(a, b, result))
+        print("Inside result: {}".format(result))
     return result
