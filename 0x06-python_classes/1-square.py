01d97f3723208cb5d919450c1eb8e4d61b0cddce
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-24 22:30:26 +0100

    All good

--- 0x06-python_classes/1-square.py
+++ 0x06-python_classes/1-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-class Square:
+"""Square class definition"""
 
+
+class Square:
+    """Represent a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
