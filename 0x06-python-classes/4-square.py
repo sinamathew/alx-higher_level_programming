c421d098590c282dea043b613daf9ddceff4c0b2
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-25 04:33:04 +0100

    4 square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' module for the `Square Class '''
+
+
+class Square:
+    ''' A Sqaure; a shape whose sides are all of the same length '''
+    def __init__(self, size=0):
+        '''  Square constructor '''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        ''' gets the length of a side of a square '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' sets the length of the sides of a square '''
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
