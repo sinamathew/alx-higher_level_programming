cb5fbe6dd01932b8b2865083f62de249ef3f66d2
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-01 21:30:23 +0100

    Nothing_to_say

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,9 +7,6 @@ class LockedClass:
     Attributes:
         __slots__ (tuple): A tuple containing the allowed attribute names.
 
-    Args:
-        first_name (str): The value for th 'first_name' attribute.
-
     Note:
         Attempting to create or access attributes not listed in __slots__ will
             results in an AttributeError
@@ -17,16 +14,3 @@ class LockedClass:
     """
 
     __slots__ = ('first_name')
-
-    def __init__(self, first_name):
-        """Initialize the LockedClass instance.
-
-        Args:
-            first_name (str): the value for the 'first_name' attribute.
-
-        Raises:
-            AttributeError: if any attributes other than 'first_name' are
-                accessed or created.
-
-        """
-        self.first_name = first_name
