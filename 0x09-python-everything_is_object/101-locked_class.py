da86b8d6e9cf73a5110897c4be9c9cebbe271ccb
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-01 21:36:02 +0100

    fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+"""Defines a locked class"""
+
+
 class LockedClass:
     """A class with no class or object attribute.
-
     Prevents the user from dynamically creating new instances attribute.
-
-    Attributes:
-        __slots__ (tuple): A tuple containing the allowed attribute names.
-
-    Note:
-        Attempting to create or access attributes not listed in __slots__ will
-            results in an AttributeError
-
     """
 
     __slots__ = ('first_name')
