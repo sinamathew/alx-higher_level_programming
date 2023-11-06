00a9ff397bf21f23199aba4c76afa0b31a4cd94a
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-07 00:11:23 +0100

    HotFix

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """InheritRectangle from (9-rectangle.py)."""
+    def __init__(self, size):
+        """Instantiation with size.
+        Args:
+            size (int): the size of the square.
+        """
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """Defines the area of the square.
+        Returns:
+            the value of the area.
+        """
+        return self.__size * self.__size
