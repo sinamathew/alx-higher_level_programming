75a56b88735053762e0b54143b5f79b832c5d0f8
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-06 21:39:26 +0100

    HotFix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,7 @@ def inherits_from(obj, a_class):
     Return:
         True if object is a subclass, else False
     """
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
