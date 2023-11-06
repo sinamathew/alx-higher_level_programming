549601c00f95d95cbbaa36b0e249ea37f7f82005
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-06 21:49:35 +0100

    Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains a class to raise exception."""
+
+
+class BaseGeometry:
+    """Just another class that raise exception."""
+    def area(self):
+        """Raises an Exception."""
+        raise Execption("area() is not implemented")
