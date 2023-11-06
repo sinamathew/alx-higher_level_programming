f72864d1050d685574f126f34ffe88e9106a5ac4
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-06 22:04:37 +0100

    All_Good

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains a class to raise exception."""
+
+
+class BaseGeometry:
+    """Just another class that raise exception and many more."""
+    def area(self):
+        """Raises an Exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """raise value and type errors on conditions."""
+        self.name = name
+        self.value = value
+
+        if not isinstance(value, int):
+            raise TypeError("<{}> must be an integer".format(self.name))
+        if value =< 0:
+            raise ValueError("<{}> must be greater than 0".format(self.name))
