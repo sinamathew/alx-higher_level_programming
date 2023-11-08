d2a7f8f5624e8a3a5be03881fd41ce63b201e743
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-08 03:55:46 +0100

    all is well

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """A scripts that adds all arguments."""
 import sys
-import json
-from os import path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
@@ -13,15 +11,13 @@ def add_item():
     Args:
         Arguments taken in CLI mode.
     """
-    filename = "add_item.json"
     try:
-        current_list = load_from_json_file(filename)
+        current_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
         current_list = []
 
-    arg_list = sys.argv[1:]
-    current_list.extend(arg_list)
-    save_to_json_file(current_list, filename)
+    current_list.extend(sys.argv[1:])
+    save_to_json_file(current_list, "add_item.json")
 
 
 if __name__ == "__main__":
