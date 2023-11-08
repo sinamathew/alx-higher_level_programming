82f8ded481cd5838a51ec7009ea25fcc3c758e0c
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-08 02:47:55 +0100

    Checking...

---
+++ 0x0B-python-input_output/5-save_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function that write object to a text file."""
+import json
+
+
+def save_to_json(my_obj, filename):
+    """Writes an Object to a text file.
+    Args:
+        my_obj (obj): the object to be written
+        filename (str): the name of the file.
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(json.dumps(my_obj)
