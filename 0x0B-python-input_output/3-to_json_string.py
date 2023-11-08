44e21a26da706059bab9f5a246424ebaf6bbe6bb
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-08 02:28:19 +0100

    JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function that returns JSON representation."""
+import json
+
+
+def to_json_string(my_obj):
+    """Just to return, nothing more.
+    Args:
+        my_obj (obj): an object (string).
+    Returns:
+        The JSON representation of an object.
+    """
+    returns json.dumps(my_obj)
