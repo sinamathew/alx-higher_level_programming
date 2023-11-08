09b12b4cc6d70c2949f29985a8e8dcd2ef501f48
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-08 03:35:23 +0100

    MyBest

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,26 @@
 """A scripts that adds all arguments."""
 import sys
 import json
+from os import path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def add_item:
+def add_item():
     """Add al argument to a Python list.
     Save the item to a file.
     Args:
         Arguments taken in CLI mode.
     """
     arg_list = sys.argv[1:]
-    save_to_json_file(arg_list, add_item.json)
+    filename = "add_item.json"
+
+    if not path.exists(filename):
+        with open(filename, 'w', encoding="utf-8") as file:
+            file.write("[]")
+
+    current_list = load_from_json(filename)
+
+    current_list.extend(arg_list)
+
+    save_to_json_file(current_list, filename)
