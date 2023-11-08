209e80714673a7a7151a5befcb8aa8fc4664b829
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-08 02:17:41 +0100

    AllGood

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that append string to file."""
+
+
+def append_write(filename="", text=""):
+    """Append string to the end of a file.
+    Args:
+        filename (str): the name of file to append.
+        text (str): string to appent to the file (UTF8).
+    """
+    with open(filename, 'wa', encoding="utf-8") as file:
+        return file.write(text)
