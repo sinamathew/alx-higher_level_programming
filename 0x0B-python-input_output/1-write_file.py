652874627fdd82840cff3660c3fbc5a030408a4d
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-08 02:01:44 +0100

    WriteToAFile

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that writes string to file."""
+
+
+def write_file(filename="", text=""):
+    """Write string to a text file (UTF8).
+    Args:
+        filename (str): the name of the file to write.
+        text (str): strings to write in the file.
+    Returns:
+        The number of character written.
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(text)
