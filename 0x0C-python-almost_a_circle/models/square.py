0671faacc74506768e39c25daad7b6335d28a9f3
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-11 21:47:28 +0100

    Task11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,6 +16,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Prints the...I don't know.
         Returns: the string.....whatever.
