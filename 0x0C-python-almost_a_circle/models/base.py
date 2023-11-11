6e9f2442b0aa1bdc7972c6e43e7653e0e6c0c38e
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-11 22:38:50 +0100

    Fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries."""
         if list_dictionaries:
-            return json.loads(list_dictionaries)
+            return json.dumps(list_dictionaries)
         else:
             return "[]"
