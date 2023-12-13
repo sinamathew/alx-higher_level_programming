5a3b11706a297ceb3d21dd9c49c21088e6d2ae27
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-12-13 03:34:20 +0100

    first_table

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,3 @@
 -- 3-list_tables.sql
-
--- Assuming the first argument is the database name
-SET @dbname = QUOTE(ARGV[1]);
-
 -- List all tables in the specified database
-USE @dbname;
 SHOW TABLES;
-
