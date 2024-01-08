1ded2b6f347e4ed1d724ae50aafe5d789eed09e3
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-01-08 22:15:20 +0100

    HotFix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const c = 'C is fun';
-const py = 'Python is cool';
-const js = 'Javascript is amazing';
-console.log(c);
-console.log(py);
-console.log(js);
+const message = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(message);
