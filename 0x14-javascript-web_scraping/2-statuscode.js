6b808d8a5581586adaca0d32dfa79655defa7564
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-03-26 23:52:52 +0100

    Fixed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, response) => {
+  if (err) {
+    console.error(err);
+  }
   console.log('Code:', response && response.statusCode);
 });
