4b88a6d4b9c7b58cc82423f7b7ee24f823a5ba03
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-03-26 23:10:34 +0100

    Fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,8 @@
 // A script that reads and prints the content of a file
 
 const fs = require('fs');
+const path = process.argv[2];
+
 fs.readFile(path, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
