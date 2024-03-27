fc86262ef574b84f215bd16e0812e0b75c68f28b
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-03-27 01:19:11 +0100

    check

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// A script that write a file from a webpage response
+
+const fs = require('fs');
+const request require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(error);
+  } else {
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }});
+}});
