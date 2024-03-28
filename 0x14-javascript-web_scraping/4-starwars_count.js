2abfa5b275af3bcea3cd3d32d21938a092f8da37
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-03-28 05:55:28 +0100

    checked

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,12 @@
 #!/usr/bin/node
-// A script that counts the number of Star Wars characters with ID 18
-// in a movie based on its episode number.
-
 const request = require('request');
-const url = process.argv[2]; // Episode number URL
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const jsonData = JSON.parse(body);
-
-    let characterCount = 0; // Initialize character count
-
-    // Check if request was successful and data exists
-    if (jsonData && jsonData.results) {
-      for (const result of jsonData.results) {
-        if (result.characters.includes(18)) {
-          characterCount++;
-        }
-      }
-      console.log(characterCount);
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
