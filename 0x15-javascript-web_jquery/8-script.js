5fb10b8dace346c4a4e11fbb886495206772e099
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-04-03 22:31:44 +0100

    Clean up mess

--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,6 +0,0 @@
-const url = 'https://swapi-api.alx-tools.com/api/films/?format=json'
-$.get(url, function (data) {
-  for (const movie of data.results) {
-    $('UL#list_movies').append('<li>' + movie.title + '</li>');
-  }
-});
